e26396de32c6a41a920e324a9050093c7e1eac7b
xiwangly2 <1334850101@qq.com>
2023-09-05 16:02:52 +0800

    重构 -node

---
+++ internal/database/mysql/xwbot.sql
@@ -0,0 +1,57 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80012
+ Source Host           : localhost:3306
+ Source Schema         : xwbot
+
+ Target Server Type    : MySQL
+ Target Server Version : 80012
+ File Encoding         : 65001
+
+ Date: 05/09/2023 13:47:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for logs
+-- ----------------------------
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs`
+(
+    `id`   bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `json` json                NOT NULL,
+    `time` datetime            NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for switch
+-- ----------------------------
+DROP TABLE IF EXISTS `switch`;
+CREATE TABLE `switch`
+(
+    `group_id` int(11)                                                       NOT NULL,
+    `switch`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `time`     datetime                                                      NOT NULL,
+    PRIMARY KEY (`group_id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of switch
+-- ----------------------------
+INSERT INTO `switch`
+VALUES (0, '1', '2023-09-05 11:05:40');
+
+SET FOREIGN_KEY_CHECKS = 1;
