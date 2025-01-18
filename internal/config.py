4a30d510b83a8ccb72273b10c5a4366172120ac6
xiwangly2 <xiwangly2@gmail.com>
2025-01-18 14:09:53 +0800

    提交，

--- internal/config.py
+++ internal/config.py
@@ -8,6 +8,6 @@ if os.path.exists('config/config.yml'):
     with open('config/config.yml', 'r', encoding='utf-8') as f:
         config = yaml.load(f.read(), Loader=yaml.FullLoader)
 else:
-    print_warning("Warning: No configuration file found, the example file will be used.")
+    print_warning("No configuration file found, the example file will be used.")
     with open('config/config_example.yml', 'r', encoding='utf-8') as f:
         config = yaml.load(f.read(), Loader=yaml.FullLoader)
